fb1d34b12c81cae79655225562134d495884bd1f
Fran Real <fran.real.perez@gmail.com>
2017-09-20 23:48:50 +0200

    Clean up

--- gazebo_animator/include/gazebo_animator/joint_animator_gazebo_plugin.h
+++ gazebo_animator/include/gazebo_animator/joint_animator_gazebo_plugin.h
@@ -3,91 +3,73 @@
 // Based on John Hsu's GazeboRosJointTrajectory plugin
 //----------------------------------------------------------------------------------------------------------------------
 #ifndef GAZEBO_ANIMATOR_GAZEBO_PLUGIN_H
- #define GAZEBO_ANIMATOR_GAZEBO_PLUGIN_H
- 
+#define GAZEBO_ANIMATOR_GAZEBO_PLUGIN_H
+
  #include <ros/ros.h>
  #include <ros/callback_queue.h>
- #include <ros/advertise_options.h>
  #include <ros/subscribe_options.h>
  #include <sensor_msgs/JointState.h>
- #include <geometry_msgs/Pose.h>
  #include <gazebo/physics/physics.hh>
- #include <gazebo/transport/TransportTypes.hh>
  #include <gazebo/common/Time.hh>
  #include <gazebo/common/Plugin.hh>
  #include <gazebo/common/Events.hh>
- 
  #include <boost/thread.hpp>
  #include <boost/thread/mutex.hpp>
- 
- namespace gazebo {
- 
+
+namespace gazebo {
+
 class JointAnimatorGazeboPlugin : public ModelPlugin {
-    /// \brief Constructor
-    public: JointAnimatorGazeboPlugin();
+public:
 
-    /// \brief Destructor
-    public: virtual ~JointAnimatorGazeboPlugin();
+    JointAnimatorGazeboPlugin();
 
-    /// \brief Load the controller
-    public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+    virtual ~JointAnimatorGazeboPlugin();
 
-    /// \brief Update the controller
-    private: void SetJointState(const sensor_msgs::JointState::ConstPtr& _joint_state);
+    void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
-    private: void UpdateStates();
+private:
 
-    private: physics::WorldPtr world_;
-    private: physics::ModelPtr model_;
+    void SetJointState(const sensor_msgs::JointState::ConstPtr& _joint_state);
+
+    void UpdateStates();
+
+    physics::WorldPtr world_;
+    physics::ModelPtr model_;
 
     /// \brief pose should be set relative to this link (default to "world")
-    private: physics::LinkPtr reference_link_;
-    private: std::string reference_link_name_;
-    /// \brief frame transform name, should match link name
-    //private: std::string tf_frame_name_;
+    physics::LinkPtr reference_link_;
+    std::string reference_link_name_;
 
     /// \brief pointer to ros node
-    private: ros::NodeHandle* rosnode_;
-    private: ros::Subscriber sub_;
-//    private: ros::ServiceServer srv_;
-//    private: bool has_trajectory_;
-
-    /// \brief ros message
-//    private: trajectory_msgs::JointTrajectory trajectory_msg_;
-//    private: bool set_model_pose_;
-//    private: geometry_msgs::Pose model_pose_;
+    ros::NodeHandle* rosnode_;
+    ros::Subscriber sub_;
 
     /// \brief topic name
-    private: std::string topic_name_;
-//    private: std::string service_name_;
+    std::string topic_name_;
 
     /// \brief A mutex to lock access to fields that are used in message callbacks
-    private: boost::mutex update_mutex;
+    boost::mutex update_mutex;
 
     /// \brief save last_time
-    private: common::Time last_time_;
-
-    // trajectory time control
-//    private: common::Time trajectory_start;
-//    private: unsigned int trajectory_index;
+    common::Time last_time_;
 
     // rate control
-    private: double update_rate_;
-    private: bool disable_physics_updates_;
-    private: bool physics_engine_enabled_;
+    double update_rate_;
+    bool disable_physics_updates_;
+    bool physics_engine_enabled_;
 
     /// \brief for setting ROS name space
-    private: std::string robot_namespace_;
+    std::string robot_namespace_;
 
-    private: ros::CallbackQueue queue_;
-    private: void QueueThread();
-    private: boost::thread callback_queue_thread_;
+    ros::CallbackQueue queue_;
+    void QueueThread();
+    boost::thread callback_queue_thread_;
 
-    private: std::vector<gazebo::physics::JointPtr> joints_;
-    private: std::vector<double> positions_;
+    std::vector<gazebo::physics::JointPtr> joints_;
+    std::vector<double> positions_;
 
     // Pointer to the update event connection
-    private: event::ConnectionPtr update_connection_;
+    event::ConnectionPtr update_connection_;
 }; 
 
 }  // namespace gazebo
