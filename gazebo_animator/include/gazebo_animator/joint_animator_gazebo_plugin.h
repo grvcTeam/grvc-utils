d9880decc9c4e3c8939f8423e6425b73e3df2712
Fran Real <fran.real.perez@gmail.com>
2017-09-21 15:04:11 +0200

    Keep it simple!

--- gazebo_animator/include/gazebo_animator/joint_animator_gazebo_plugin.h
+++ gazebo_animator/include/gazebo_animator/joint_animator_gazebo_plugin.h
@@ -5,16 +5,14 @@
 #ifndef GAZEBO_ANIMATOR_GAZEBO_PLUGIN_H
 #define GAZEBO_ANIMATOR_GAZEBO_PLUGIN_H
 
- #include <ros/ros.h>
- #include <ros/callback_queue.h>
- #include <ros/subscribe_options.h>
- #include <sensor_msgs/JointState.h>
- #include <gazebo/physics/physics.hh>
- #include <gazebo/common/Time.hh>
- #include <gazebo/common/Plugin.hh>
- #include <gazebo/common/Events.hh>
- #include <boost/thread.hpp>
- #include <boost/thread/mutex.hpp>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+#include <sensor_msgs/JointState.h>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/Time.hh>
+#include <gazebo/common/Plugin.hh>
+#include <boost/thread.hpp>
 
 namespace gazebo {
 
@@ -31,45 +29,19 @@ private:
 
     void SetJointState(const sensor_msgs::JointState::ConstPtr& _joint_state);
 
-    void UpdateStates();
-
     physics::WorldPtr world_;
     physics::ModelPtr model_;
 
-    /// \brief pose should be set relative to this link (default to "world")
-    physics::LinkPtr reference_link_;
-    std::string reference_link_name_;
+    common::Time last_time_;
 
-    /// \brief pointer to ros node
     ros::NodeHandle* rosnode_;
     ros::Subscriber sub_;
 
-    /// \brief topic name
-    std::string topic_name_;
-
-    /// \brief A mutex to lock access to fields that are used in message callbacks
-    boost::mutex update_mutex;
-
-    /// \brief save last_time
-    common::Time last_time_;
-
-    // rate control
-    double update_rate_;
-    bool disable_physics_updates_;
-    bool physics_engine_enabled_;
-
-    /// \brief for setting ROS name space
-    std::string robot_namespace_;
-
-    ros::CallbackQueue queue_;
     void QueueThread();
+    ros::CallbackQueue queue_;
     boost::thread callback_queue_thread_;
 
-    std::vector<gazebo::physics::JointPtr> joints_;
-    std::vector<double> positions_;
-
-    // Pointer to the update event connection
-    event::ConnectionPtr update_connection_;
+    std::map<std::string, double> joint_positions_;
 }; 
 
 }  // namespace gazebo
--- gazebo_animator/src/joint_animator_gazebo_plugin.cpp
+++ gazebo_animator/src/joint_animator_gazebo_plugin.cpp
@@ -6,13 +6,9 @@
 
 namespace gazebo {
 
-JointAnimatorGazeboPlugin::JointAnimatorGazeboPlugin() {
-    this->physics_engine_enabled_ = true;
-    this->disable_physics_updates_ = true;
-}
+JointAnimatorGazeboPlugin::JointAnimatorGazeboPlugin() {}
 
 JointAnimatorGazeboPlugin::~JointAnimatorGazeboPlugin() {
-    event::Events::DisconnectWorldUpdateBegin(this->update_connection_);
     this->rosnode_->shutdown();
     this->queue_.clear();
     this->queue_.disable();
@@ -22,28 +18,17 @@ JointAnimatorGazeboPlugin::~JointAnimatorGazeboPlugin() {
 
 void JointAnimatorGazeboPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
     this->model_ = _model;
-
-    // Get the world
     this->world_ = _model->GetWorld();
 
-    // load parameters
-    this->robot_namespace_ = "";
+    std::string robot_namespace = "";
     if (_sdf->HasElement("robotNamespace")) {
-        this->robot_namespace_ = _sdf->GetElement("robotNamespace")->GetValue()->GetAsString() + "/";
+        robot_namespace = _sdf->GetElement("robotNamespace")->GetValue()->GetAsString() + "/";
     }
 
-    if (!_sdf->HasElement("topicName")) {
-        this->topic_name_ = "set_joint_state";
-    } else {
-        this->topic_name_ = _sdf->GetElement("topicName")->GetValue()->GetAsString();
+    std::string topic_name = "set_joint_state";
+    if (_sdf->HasElement("topicName")) {
+        topic_name = _sdf->GetElement("topicName")->GetValue()->GetAsString();
     }
-
-    // if (!_sdf->HasElement("updateRate")) {
-    //     ROS_INFO("joint animator plugin missing <updateRate>, defaults to 0.0 (as fast as possible)");
-    //     this->update_rate_ = 0;
-    // } else {
-    //     _sdf->GetElement("updateRate")->GetValue()->Get(this->update_rate_);
-    // }
 
     if (!ros::isInitialized()) {
         int argc = 0;
@@ -52,61 +37,24 @@ void JointAnimatorGazeboPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _
         gzwarn << "should start ros::init in simulation by using the system plugin\n";
     }
 
-    this->rosnode_ = new ros::NodeHandle(this->robot_namespace_);
-
-    if (this->topic_name_ != "") {
-        ros::SubscribeOptions joint_state_so = ros::SubscribeOptions::create<sensor_msgs::JointState>(
-            this->topic_name_, 10, boost::bind(&JointAnimatorGazeboPlugin::SetJointState, this, _1),
-            ros::VoidPtr(), &this->queue_);
-        this->sub_ = this->rosnode_->subscribe(joint_state_so);
-    }
+    this->rosnode_ = new ros::NodeHandle(robot_namespace);
+    ros::SubscribeOptions joint_state_so = ros::SubscribeOptions::create<sensor_msgs::JointState>(
+        topic_name, 10, boost::bind(&JointAnimatorGazeboPlugin::SetJointState, this, _1),
+        ros::VoidPtr(), &this->queue_);
+    this->sub_ = this->rosnode_->subscribe(joint_state_so);
 
     this->last_time_ = this->world_->GetSimTime();
 
     // start custom queue for joint state plugin ros topics
     this->callback_queue_thread_ = boost::thread(boost::bind(&JointAnimatorGazeboPlugin::QueueThread, this));
-
-    // New Mechanism for Updating every World Cycle
-    // Listen to the update event. This event is broadcast every
-    // simulation iteration.
-    this->update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&JointAnimatorGazeboPlugin::UpdateStates, this));
 }
 
 void JointAnimatorGazeboPlugin::SetJointState(const sensor_msgs::JointState::ConstPtr& _joint_state) {
-    boost::mutex::scoped_lock lock(this->update_mutex);
     // copy joint configuration into a map
-    unsigned int chain_size = _joint_state->name.size();
-    this->joints_.resize(chain_size);
-    this->positions_.resize(chain_size);
-    for (unsigned int i = 0; i < chain_size; ++i) {
-        this->joints_[i] = this->model_->GetJoint(_joint_state->name[i]);
-        this->positions_[i] = _joint_state->position[i];
+    for (unsigned int i = 0; i < _joint_state->name.size(); ++i) {
+        joint_positions_[_joint_state->name[i]] = _joint_state->position[i];
     }
-}
-
-
-void JointAnimatorGazeboPlugin::UpdateStates() {
-    boost::mutex::scoped_lock lock(this->update_mutex);
-    common::Time cur_time = this->world_->GetSimTime();
-
-    if (this->disable_physics_updates_) {
-        this->physics_engine_enabled_ = this->world_->GetEnablePhysicsEngine();
-        this->world_->EnablePhysicsEngine(false);
-    }
-
-    for (unsigned int i = 0; i < this->joints_.size(); ++i) {
-        // this is not the most efficient way to set things
-        if (this->joints_[i]) {
-            this->joints_[i]->SetPosition(0, this->positions_[i]);
-        }
-    }
-
-    // save last update time stamp
-    this->last_time_ = cur_time;
-
-    if (this->disable_physics_updates_) {
-        this->world_->EnablePhysicsEngine(this->physics_engine_enabled_);
-    }
+    model_->SetJointPositions(joint_positions_);
 }
 
 void JointAnimatorGazeboPlugin::QueueThread() {
