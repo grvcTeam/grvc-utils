3cb7c74243c4215fa2b5d91b7fdf2a692bd3edb2
alif <alif@grvc.us.es>
2018-05-25 17:07:16 +0200

    Fixed measure filter

--- vicon_bridge/scripts/tsbridge.py
+++ vicon_bridge/scripts/tsbridge.py
@@ -13,7 +13,8 @@ import struct
 
 
 class LeicaThread(Thread):
-    cBufferSize = 44
+    cBufferSize = 24
+    OFFSET=0.5
 
     def __init__(self, _ip, _port):
         Thread.__init__(self)
@@ -29,36 +30,47 @@ class LeicaThread(Thread):
         # self.mIsconnected = False
         self.mRun = True
         self.mLastTime = 0
+        self.TSerror = False
 
     def _del_(self):
         self.mRun = False
         self.mSocket.close()
 
     def run(self):
+        cMaxTimeOut = 1.0
         sent = self.mSocket.sendto("Hola", self.mServer)
         self.mLastTime = time.time()
 
         while (not rospy.is_shutdown()) and self.mRun:
             data = self.mSocket.recvfrom(LeicaThread.cBufferSize)
-            self.mLastTime = time.time()
             if not data:
                 print "No data received, disconnected from server as socket has been configured without timeout"
             else:
-                data = struct.unpack('L4f', data[0])
+                # rospy.loginfo("%s %s", data, len(data[0]))
+                data = struct.unpack('fffQ', data[0])
                 # sys.stdout = open('TS2PX4_log.txt','awt')
                 # print data
                 try:
-                    # print parseData
-                    self.mLastX, self.mLastY, self.mLastZ, t = data[1], data[2], data[3], data[0]
-                    # print "--------"
-                except IndexError:
+                    # Eval if any data is 0, measure delay or failed measures
+                    if(any(x is 0 for x in data) or (time.time() - self.mLastTime > cMaxTimeOut)):# or deltaFilter(data)):
+                        # Block publisher 
+                        rospy.logerr("Recived bad measurement quality %s %s", data,time.time() - self.mLastTime)
+                        self.TSerror = True
+                    else:
+                        self.mLastX, self.mLastY, self.mLastZ, t = data[0], data[1], data[2], data[3]
+                        # rospy.loginfo("%s", data)
+                        self.TSerror = False
+                    # 
+                    self.mLastTime = time.time()
+                except IndexError as error:
                     print "Captured index error while parsing input data from socket. Skipping data"
+                    print error
 
+    # def deltaFilter(lastPose,self):
+        # return abs(lastPose[1] - self.mLastX) < OFFSET or abs(lastPose[2] - self.mLastY) < OFFSET or abs(lastPose[3] - self.mLastZ) < OFFSET
 
 def talker():
     rospy.init_node('tsbridge', anonymous=True)
-    watchdogCounter = 0
-    delayFilter = 5
     output = PoseStamped()
     # TSReceiver Socket
     pub = rospy.Publisher('/uav_1/mavros/vision_pose/pose', PoseStamped, queue_size=1)
@@ -69,39 +81,22 @@ def talker():
     leicaThread.start()
 
     rate = rospy.Rate(20)  # 20hz
-    cMaxTimeOut = 1.0
 
     while not rospy.is_shutdown():
-		#Y=X=Z with lost prism TS send 0,0,0 measure
-        if (time.time() - leicaThread.mLastTime > cMaxTimeOut) or (leicaThread.mLastY == leicaThread.mLastX and leicaThread.mLastX == leicaThread.mLastZ):
-            if watchdogCounter < 5:
-                watchdogCounter = watchdogCounter + 1
-                output.header.stamp = rospy.Time.now()
-        else:
+        if(not leicaThread.TSerror):
+            pub.publish(output)
             output.header.stamp = rospy.Time.now()
-            watchdogCounter = 0
+            output.header.frame_id = "fcu"
 
-        if delayFilter == 0:
-            if (abs(output.pose.position.x - leicaThread.mLastY) < 0.4 or abs(output.pose.position.y - leicaThread.mLastX) < 0.4 or abs(output.pose.position.z - leicaThread.mLastZ) < 0.4):
-                # NED Conversion
-                output.pose.position.x = leicaThread.mLastY
-                output.pose.position.y = leicaThread.mLastX
-                output.pose.position.z = -leicaThread.mLastZ
-        else:
-            delayFilter = delayFilter - 1
-            # NED Conversion
-            output.pose.position.x = leicaThread.mLastY
-            output.pose.position.y = leicaThread.mLastX
-            output.pose.position.z = -leicaThread.mLastZ
+            output.pose.position.x = leicaThread.mLastX
+            output.pose.position.y = leicaThread.mLastY
+            output.pose.position.z = leicaThread.mLastZ
 
-        # output.header.stamp = rospy.Time.now()
-        output.header.frame_id = "fcu"
-        output.pose.orientation.x = 0
-        output.pose.orientation.y = 0
-        output.pose.orientation.z = 0
-        output.pose.orientation.w = 1
-
-        pub.publish(output)
+            output.pose.orientation.x = 0
+            output.pose.orientation.y = 0
+            output.pose.orientation.z = 0
+            output.pose.orientation.w = 1
+            
         rate.sleep()
 
 if __name__ == '__main__':
